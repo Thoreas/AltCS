15b6fb8d0087031bbf622c180f25014cf7e632a9
Thoreas <44213940+Thoreas@users.noreply.github.com>
2018-10-24 12:06:28 +0200

    Removed debug outputs

--- js/scripts.js
+++ js/scripts.js
@@ -56,10 +56,8 @@ for ( id in idsRelevantForCalculatingCharacterSkills ) {
 
 // Discard invalid keys for skills
 function discardInvalidKeysForSkills(e) {
-	console.log("tu sam");
 	// Allow only numbers, backspace, delete, tab, left arrow and right arrow keys; discard everything else
 	if ( e.which != 0 && e.which != 8 && e.which != 9 && e.which != 37 && e.which != 39 && e.which != 46 && !(e.which >= 48 && e.which <= 57) ) {
-		console.log(e.which + " odbacio");
 		e.preventDefault();
 		return;
 	}
